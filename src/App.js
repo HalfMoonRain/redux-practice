7de420d00194f4bc43c0a8c97eee8c2d71744131
JunWoo Lee <junwoo1357@gmail.com>
2024-03-19 20:30:25 +0900

    redux 셋업, 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  const increase = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={increase}> + </button>
+      <h1>count : {count}</h1>
     </div>
   );
 }
